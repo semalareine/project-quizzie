5b3295ff4d780cfbf860a398d6f6964fd0d23f4e
Alex <103825@glr.nl>
2026-03-27 13:56:45 +0100

    bug fix 2, categorie page added

---
+++ quizzpagina/main.js
@@ -0,0 +1,21 @@
+
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.getElementById('nav');
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+
+document.addEventListener('click', (e) => {
+    if (!nav.contains(e.target) && !menuBtn.contains(e.target)) {
+        nav.classList.remove('active');
+    }
+});
+
+const navLinks = nav.querySelectorAll('a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
+});
